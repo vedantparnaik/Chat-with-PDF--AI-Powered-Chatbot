e1cc003d5e55cebf20e3cf2cc16018659e04196b
vedantparnaik <81671190+vedantparnaik@users.noreply.github.com>
2024-11-09 18:36:39 -0500

    Update app.py
    updated by chroma

--- app.py
+++ app.py
@@ -9,11 +9,12 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-
+from langchain.vectorstores import Chroma
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 
 def get_pdf_text(pdf_docs):
     text=""
@@ -23,6 +24,7 @@ def get_pdf_text(pdf_docs):
             text+= page.extract_text()
     return  text
 
+
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
@@ -30,24 +32,12 @@ def get_text_chunks(text):
 
 
 def get_vector_store(text_chunks):
-
-    os.makedirs("faiss_index", exist_ok=True)
-
-    print("============================================================")
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("/Users/vedantparnaik/chatbot/faiss_index")
-    print("FAISS index saved successfully.")
+    vector_store = Chroma.from_texts(text_chunks, embedding=embeddings, persist_directory="./chroma_db")
+    # Save the vector store
+    vector_store.persist()
+    print("ChromaDB index saved successfully.")
 
-    
-    '''
-    try:
-        vector_store.save_local("faiss_index")
-        print("FAISS index saved successfully.")
-    except Exception as e:
-        print(f"Failed to save FAISS index: {e}")
-
-    '''
 
 def get_conversational_chain():
 
@@ -72,12 +62,11 @@ def get_conversational_chain():
 
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
-    new_db = FAISS.load_local("/Users/vedantparnaik/chatbot/faiss_index", embeddings, allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_question)
+    vector_store = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+    # new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = vector_store.similarity_search(user_question)
 
     chain = get_conversational_chain()
-
     
     response = chain(
         {"input_documents":docs, "question": user_question}
@@ -91,7 +80,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with multiple PDF")
+    st.header("Chat with PDF using Gemini💁")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
